b60d05f46a72e44c840facea0531f5810d697a8e
Stephanie Thompson <43892794+Rocklobster84@users.noreply.github.com>
2025-06-02 17:35:16 -0500

    Initial commit plus some work on the hero component

---
+++ components/Hero.tsx
@@ -0,0 +1,18 @@
+const Hero = () => {
+
+  return (
+    <div className="text-center">
+      <hr className="px-40" />
+      <h1>HELLO, I'M STEPHANIE</h1>
+      <h3>FULL STACK DEVELOPER</h3>
+      <hr />
+
+      <p className="text-2xl flex-wrap flex-shrink px-10 py-40">
+        10+ years of experience with designing building, and deploying web applications utilizing various technologies and programming languages.
+      </p>
+
+    </div>
+  )
+}
+
+export default Hero
