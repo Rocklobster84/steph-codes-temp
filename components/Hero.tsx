ea81ca69ad2561a9ba20acec0ef5fa0ea1a1e740
Stephanie Thompson <43892794+Rocklobster84@users.noreply.github.com>
2025-06-03 15:02:13 -0500

    Made some minor changes to Heros section styling

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,12 +2,12 @@ const Hero = () => {
 
   return (
     <div className="text-center">
-      <hr className="mt-20 flex flex-grow border-t border-white-300 w-4/5 m-auto"/>
-      <h1 className="px-10 my-10 uppercase">Hi, I&#39;m Stephanie</h1>
+      <div className="mx-auto w-[calc(100%_+_2rem)] max-w-xl h-1 mt-20 bg-white" />
+      <h1 className="px-10 my-10 uppercase max-w-2xl mx-auto">Hi, I&#39;m Stephanie</h1>
       <h3 className="mb-10 md:w-full uppercase">Full Stack Developer</h3>
-      <hr className="flex flex-grow border-t border-white-300 w-4/5 m-auto"/>
+      <div className="mx-auto w-[calc(100%_+_2rem)] max-w-xl h-1 bg-white" />
 
-      <p className="text-xl md:text-2xl flex-wrap flex-shrink px-10 py-20 w-4/5 m-auto">
+      <p className="text-xl md:text-2xl flex-wrap flex-shrink px-10 py-20 md:w-[calc(40%_+_2rem)] m-auto">
         10+ years of experience with designing building, and deploying web applications utilizing various technologies and programming languages.
       </p>
 
