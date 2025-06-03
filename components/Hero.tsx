0d1ed4afc10ec9694161bc9ab92f171743cf3245
Stephanie Thompson <43892794+Rocklobster84@users.noreply.github.com>
2025-06-03 13:06:31 -0500

    Hero section nearly completed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,12 +2,12 @@ const Hero = () => {
 
   return (
     <div className="text-center">
-      <hr className="px-40" />
-      <h1>HELLO, I'M STEPHANIE</h1>
-      <h3>FULL STACK DEVELOPER</h3>
-      <hr />
+      <hr className="mt-20 flex flex-grow border-t border-white-300 w-4/5 m-auto"/>
+      <h1 className="px-40 my-10 uppercase">Hi, I'm Stephanie</h1>
+      <h3 className="mb-10 md:w-full uppercase">Full Stack Developer</h3>
+      <hr className="flex flex-grow border-t border-white-300 w-4/5 m-auto"/>
 
-      <p className="text-2xl flex-wrap flex-shrink px-10 py-40">
+      <p className="text-xl md:text-2xl flex-wrap flex-shrink px-10 py-20 w-4/5 m-auto">
         10+ years of experience with designing building, and deploying web applications utilizing various technologies and programming languages.
       </p>
 
