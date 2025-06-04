3b66a65be8516d6aaf1f685620b8dcf73f970715
Stephanie Thompson <43892794+Rocklobster84@users.noreply.github.com>
2025-06-04 13:04:22 -0500

    Updated MyWork component to correct some styling issues. Knocked it down to one entry to prepare for mapping

--- components/MyWork.tsx
+++ components/MyWork.tsx
@@ -1,15 +1,16 @@
 "use client";
-import * as React from "react";
+
+import Image from "next/image"; 
 
 function MyWork() {
   return (
-    <div className="flex flex-col mx-auto px-10 w-full max-w-sm md:max-w-2xl">
+    <div className="flex flex-col mx-auto w-full max-w-sm md:max-w-2xl">
       <div className="shrink-0 self-end max-w-full h-0 border-white border-solid border-[1px] w-full" />
       <div className="flex flex-nowrap gap-5 justify-between w-full max-md:max-w-full">
         <div className="flex flex-col max-md:max-w-full">
           <div className="flex gap-5 justify-between max-w-full w-[80px] md:w-[137px]">
-            <div className="shrink-0 border-white border-solid border-[1px] h-[100px] md:h-[168px] w-[3px]" />
-            <div className="shrink-0 border-white border-solid border-[1px] h-[100px] md:h-[168px] w-[3px]" />
+            <div className="shrink-0 border-white bg-white border-solid border-[1px] h-[100px] md:h-[168px] w-[3px]" />
+            <div className="shrink-0 border-white bg-white border-solid border-[1px] h-[100px] md:h-[168px] w-[3px]" />
           </div>
           <div className="flex flex-col self-end w-full max-w-[1325px] max-md:max-w-full">
             <div className="titles self-start text-4xl font-bold text-white md:max-w-full md:text-6xl mt-10 uppercase">
@@ -19,11 +20,22 @@ function MyWork() {
               TODO: SUBTITLE
             </div>
             <div className="mt-28 max-md:mt-10 max-md:mr-1.5 max-md:max-w-full">
-              <div className="flex gap-5 max-md:flex-col">
+              <div className="flex gap-5 items-start max-md:flex-col">
+                <div className="w-6/12 max-md:ml-0 max-md:w-full">
+                  <div className="flex flex-col grow justify-center items-start px-10 py-8 w-full bg-accent max-md:px-5 max-md:mt-5 max-md:max-w-full">
+                    <Image
+                      src="/istockphoto-1147544807-612x612.png"
+                      width={451}
+                      height={215}
+                      alt="Project Image"
+                      className="object-contain max-w-full aspect-[2.1] w-[451px]"
+                    />
+                  </div>
+                </div>
                 
                 <div className="ml-5 w-6/12 max-md:ml-0 max-md:w-full">
-                  <div className="flex flex-col mt-10 text-4xl text-white max-md:mt-10 max-md:max-w-full">
-                    <div className="self-start text-center titles font-bold">
+                  <div className="flex flex-col text-4xl text-white mt-10 md:mt-0 max-md:max-w-full">
+                    <div className="self-start text-center titles font-bold uppercase">
                       HEADING
                     </div>
                     <p className="mt-4 md:max-w-full text-xl">
@@ -34,63 +46,6 @@ function MyWork() {
                     </p>
                   </div>
                 </div>
-
-              <div className="w-6/12 max-md:ml-0 max-md:w-full">
-                  <div className="flex flex-col grow justify-center items-center px-20 py-12 w-full bg-accent max-md:px-5 max-md:mt-5 max-md:max-w-full">
-                    <img
-                      src="https://cdn.builder.io/api/v1/image/assets/TEMP/5c03c1d2d660e49d0e592a050cae617c0dcc63d4?placeholderIfAbsent=true"
-                      className="object-contain max-w-full aspect-[2.1] w-[451px]"
-                    />
-                  </div>
-                </div>
-
-        
-                <div className="ml-5 w-6/12 max-md:ml-0 max-md:w-full">
-                  <div className="flex flex-col mt-10 text-4xl text-white max-md:mt-10 max-md:max-w-full">
-                    <div className="self-start text-center titles font-bold">
-                      HEADING
-                    </div>
-                    <p className="mt-4 md:max-w-full text-xl">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut
-                    </p>
-                  </div>
-                </div>
-             
-              <div className="w-6/12 max-md:ml-0 max-md:w-full">
-                <div className="flex flex-col grow justify-center items-center px-20 py-12 w-full bg-accent max-md:px-5 max-md:mt-5 max-md:max-w-full">
-                  <img
-                      src="https://cdn.builder.io/api/v1/image/assets/TEMP/5c03c1d2d660e49d0e592a050cae617c0dcc63d4?placeholderIfAbsent=true"
-                      className="object-contain max-w-full aspect-[2.1] w-[451px]"
-                    />
-                </div>
-              </div>
-
-              <div className="ml-5 w-6/12 max-md:ml-0 max-md:w-full">
-                <div className="flex flex-col mt-10 text-4xl text-white max-md:mt-10 max-md:max-w-full">
-                  <div className="self-start text-center titles font-bold">
-                    HEADING
-                  </div>
-                    <p className="mt-4 md:max-w-full text-xl">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="w-6/12 max-md:ml-0 max-md:w-full">
-                <div className="flex flex-col grow justify-center items-center px-20 py-12 w-full bg-accent max-md:px-5 max-md:mt-5 max-md:max-w-full">
-                  <img
-                      src="https://cdn.builder.io/api/v1/image/assets/TEMP/5c03c1d2d660e49d0e592a050cae617c0dcc63d4?placeholderIfAbsent=true"
-                      className="object-contain max-w-full aspect-[2.1] w-[451px]"
-                    />
-                </div>
-              </div>
-
 
               </div>
             </div>
@@ -103,12 +58,10 @@ function MyWork() {
               />
             </div>
           </div>
-
-         
         
-        <div className="shrink-0 self-start w-0.5 border-white border-solid border-[1px] h-[1152px]" />
       </div>
       </div>
+    </div>
   );
 }
 
