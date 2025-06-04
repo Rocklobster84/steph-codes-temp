b49f205ef377b9920a64b04c18a2551359e76e90
Stephanie Thompson <43892794+Rocklobster84@users.noreply.github.com>
2025-06-04 14:14:46 -0500

    Updated MyWork styling to fit different screen sizes better

--- components/MyWork.tsx
+++ components/MyWork.tsx
@@ -4,13 +4,13 @@ import Image from "next/image";
 
 function MyWork() {
   return (
-    <div className="flex flex-col mx-auto w-full max-w-sm md:max-w-2xl">
-      <div className="shrink-0 self-end max-w-full h-0 border-white border-solid border-[1px] w-full" />
+    <div className="flex flex-col mx-auto max-md:max-w-3/4 lg:max-w-2/3">
+      <div className="shrink-0 self-end max-w-full h-0 border-white border-solid border-[1px] w-full sm:px" />
       <div className="flex flex-nowrap gap-5 justify-between w-full max-md:max-w-full">
         <div className="flex flex-col max-md:max-w-full">
           <div className="flex gap-5 justify-between max-w-full w-[80px] md:w-[137px]">
-            <div className="shrink-0 border-white bg-white border-solid border-[1px] h-[100px] md:h-[168px] w-[3px]" />
-            <div className="shrink-0 border-white bg-white border-solid border-[1px] h-[100px] md:h-[168px] w-[3px]" />
+            <div className="shrink-0 border-white bg-white border-solid border-[1px] h-[100px] md:h-[168px] w-[1px]" />
+            <div className="shrink-0 border-white bg-white border-solid border-[1px] h-[100px] md:h-[168px] w-[1px]" />
           </div>
           <div className="flex flex-col self-end w-full max-w-[1325px] max-md:max-w-full">
             <div className="titles self-start text-4xl font-bold text-white md:max-w-full md:text-6xl mt-10 uppercase">
