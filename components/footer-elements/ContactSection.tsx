ba6264eff118cd73b59979002c203581860a571c
Stephanie Thompson <steph@stephcodes.dev>
2025-06-26 14:10:53 -0500

    Dropped font size in footer down to medium

--- components/footer-elements/ContactSection.tsx
+++ components/footer-elements/ContactSection.tsx
@@ -7,7 +7,7 @@ import { FaLinkedin } from "react-icons/fa";
 
 export const ContactSection = () => {
   return (
-    <section className="flex grow gap-8 items-center text-lg md:text-2xl font-medium tracking-normal leading-none text-white mr-10 md:mr-0">
+    <section className="flex grow gap-8 items-center text-md md:text-2xl tracking-normal leading-none text-white mr-10 md:mr-0">
       <div className="flex flex-col grow shrink-0 items-center md:items-start my-auto basis-0 w-fit pl-10">
         <h4>Get in Touch</h4>
         <div className="mt-6">
--- components/footer-elements/NavigationSection.tsx
+++ components/footer-elements/NavigationSection.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 export const NavigationSection = () => {
   return (
-    <nav className="flex gap-6 items-center flex-col md:flex-row self-stretch my-auto text-lg md:text-2xl leading-none text-center text-white max-md:mt-10 max-md:max-w-full">
+    <nav className="flex gap-6 items-center flex-col md:flex-row self-stretch my-auto text-md md:text-2xl leading-none text-center text-white max-md:mt-10 max-md:max-w-full">
       <Link href="/">
         Home
       </Link>
